7a8d8470702f01f0942fc89664bf97c89fc7bc1d
xiaozhenano <xiaozhedesu0219@outlook.com>
2025-01-29 17:58:09 +0800

    添加信息输出开关，默认为true

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import { resolve } from 'path'
 
 export const name = 'random-yuzusoft-linestamp'
 
-export interface Config { }
+export interface Config {
+  fileShow: boolean
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  fileShow: Schema.boolean().default(true)
+    .description('输出发出的图片信息')
+})
 
 const randomArrayIndex = (length) => (length <= 0) ? 0 : Math.floor(Math.random() * (length - 1));
 
@@ -17,15 +22,15 @@ const stampsInfo = [
   { name: 'yuzusoft-tenshisouzou', length: 40 },
 ];
 
-export function apply(ctx: Context) {
+export function apply(ctx: Context, config: Config) {
   ctx.command('random-yuzusoft-linestamp')
     .action(async ({ session }) => {
       const stampInfo = stampsInfo[randomArrayIndex(stampsInfo.length)];
-      console.log(stampInfo)
       const fileName = `${stampInfo.name}-${randomArrayIndex(stampInfo.length)}.png`
-      session.send(`stamp: ${fileName}`);
+      if (config.fileShow) {
+        session.send(`stamp: ${fileName}`);
+      }
       const url = 'file:///' + resolve(__dirname, `../stamps/${stampInfo.name}/${fileName}`);
-      console.log(url)
       session.send(h('img', { src: url }));
     })
 }
