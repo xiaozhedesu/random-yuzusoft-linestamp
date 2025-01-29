a6dd8c61806528f1a6ea4c2a1ce8cdef2041c7c8
xiaozhenano <xiaozhedesu0219@outlook.com>
2025-01-29 14:11:29 +0800

    随机柚子社LINE表情

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
+import { resolve } from 'path'
 
 export const name = 'random-yuzusoft-linestamp'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
+const randomArrayIndex = (length) => (length <= 0) ? 0 : Math.floor(Math.random() * (length - 1));
+
+const stampsInfo = [
+  { name: 'yuzusoft-sanoba', length: 40 },
+  { name: 'yuzusoft-senrenbanka', length: 40 },
+  { name: 'yuzusoft-riddlejoker', length: 40 },
+  { name: 'yuzusoft-cafestella', length: 40 },
+  { name: 'yuzusoft-tenshisouzou', length: 40 },
+];
+
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('random-yuzusoft-linestamp')
+    .action(async ({ session }) => {
+      const stampInfo = stampsInfo[randomArrayIndex(stampsInfo.length)];
+      console.log(stampInfo)
+      const fileName = `${stampInfo.name}-${randomArrayIndex(stampInfo.length)}.png`
+      session.send(`stamp: ${fileName}`);
+      const url = 'file:///' + resolve(__dirname, `../stamps/${stampInfo.name}/${fileName}`);
+      console.log(url)
+      session.send(h('img', { src: url }));
+    })
 }
